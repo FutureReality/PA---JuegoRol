63cb54cf7b7e824e0925a0658e8359693fecae72
FutureReality <115637305+FutureReality@users.noreply.github.com>
2024-05-10 09:35:54 +0200

    Create objetos.java

---
+++ objetos.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/*
+{}
+clase objetos:
+	--variables--
+	definir nombre:
+		nombre = nombre;
+	definir atributos objeto:
+		a = a;
+		b = b;
+		c = c;
+	descripcion del objeto:
+		descripcion = descripcion;
+	main:
+	.
+	.
+	.
+*/
+
+public class objetos {
+	
+	static String nombre;
+	
+	static int armadura;
+	static int daño;
+	static int gasto_mana;
+	
+	static String desc;
+	
+	static void nombre() {
+		
+		
+		
+	}
+	
+	
+}
